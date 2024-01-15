2da82e616b3e325dbceea1914fcdeb8335923bcc
Ken Yuen <kenneth.x.yuen@gmail.com>
2024-01-15 11:40:53 -0600

    Added Shared Modules for React

--- mfp/packages/marketing/config/webpack.dev.js
+++ mfp/packages/marketing/config/webpack.dev.js
@@ -18,7 +18,7 @@ const devConfig = {
             exposes: {
                 './MarketingApp': './src/bootstrap'
             },
-            // shared: ['react', 'react-dom']
+            shared: ['react', 'react-dom']
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
